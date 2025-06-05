4448fc78caf405adee31ca0a108c00f4b95bfed5
Jose Luis Saelices Vila <joseluis.saelicesvila@ust.com>
2025-06-05 10:56:55 +0200

    Initial Commit

---
+++ src/test/java/com/bcnc/inditex/technical_test/TechnicalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcnc.inditex.technical_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
